c4b8ab277067882b8eaa21cb9dc6f32bbcd90a51
Dhrumil <sdhrumil2605@gmail.com>
2021-04-25 20:00:39 -0400

    teleport front end ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Login from "./components/Login";
+import Nav from "./components/Nav";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8000/api/user', {
+                    headers: {'Content-Type': 'application/json'},
+                    credentials: 'include',
+                });
+
+                const content = await response.json();
+
+                content.name&&setName(content.name);
+                console.log(content.name)
+            }
+        )();
+    });
+
+
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Nav name={name} setName={setName}/>
+
+                <main className="form-signin">
+                    <Route path="/" exact component={() => <Home name={name}/>}/>
+                    <Route path="/login" component={() => <Login setName={setName}/>}/>
+                    <Route path="/register" component={Register}/>
+                </main>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
